762ff50305a57a09cb2aae17b7c30ba4a5f746ff
chaban-dmytro <dimach@ukr.net>
2023-12-06 11:58:47 +0100

    add change avatar

--- routes/api/auth-router.js
+++ routes/api/auth-router.js
@@ -14,5 +14,11 @@ authRouter.post("/signin", isEmptyBody, authController.signIn);
 authRouter.post("/signout", authenticate, authController.signOut);
 authRouter.get("/current", authenticate, authController.getCurrent);
 authRouter.patch("/subupdate", authenticate, authController.subUpdate);
+authRouter.patch(
+  "/avatars",
+  upload.single("avatarUrl"),
+  authenticate,
+  authController.avatarUpdate
+);
 
 export default authRouter;
